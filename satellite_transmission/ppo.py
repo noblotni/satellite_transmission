58548402a06c1cc2319645bca9a4cc4cb9e242d6
Flavien Deseure--Charron <flavien.deseure@gmail.com>
2023-02-09 14:53:09 +0100

    ppo

---
+++ satellite_transmission/ppo.py
@@ -0,0 +1,133 @@
+"""Run the actor-crictic algorithm to solve the optimization problem."""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from satellite_transmission.environment import SatelliteEnv
+import matplotlib.pyplot as plt
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# NN layers
+HIDDEN_SIZE = 128
+# Discount factor
+GAMMA = 0.99
+# Learning rates
+LR_ACTOR = 0.0001
+LR_CRITIC = 0.0001
+
+
+class ActorNetwork(nn.Module):
+    def __init__(self, obs_size, action_size):
+        """Map a state to an action."""
+        super().__init__()
+        self.fc1_layer = nn.Linear(obs_size, HIDDEN_SIZE)
+        self.fc2_layer = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.mu_out_layer = nn.Sequential(
+            nn.Linear(HIDDEN_SIZE, action_size), nn.Sigmoid()
+        )
+        self.sigma_out_layer = nn.Sequential(
+            nn.Linear(HIDDEN_SIZE, action_size), nn.Softplus()
+        )
+
+    def forward(self, x):
+        x = self.fc1_layer(x)
+        x = self.fc2_layer(x)
+        mu = self.mu_out_layer(x)
+        sigma_diag = self.sigma_out_layer(x)
+        norm_dist = torch.distributions.MultivariateNormal(
+            loc=mu, covariance_matrix=torch.diag(sigma_diag)
+        )
+        x = norm_dist.sample()
+        return x, norm_dist
+
+
+class CriticNetwork(nn.Module):
+    def __init__(self, obs_size):
+        """Map a state to a quality-value."""
+        super().__init__()
+        self.fc1_layer = nn.Linear(obs_size, HIDDEN_SIZE)
+        self.fc2_layer = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.critic_out_layer = nn.Linear(HIDDEN_SIZE, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1_layer(x)
+        x = self.sigmoid(x)
+        x = self.fc2_layer(x)
+        x = self.sigmoid(x)
+        x = self.critic_out_layer(x)
+        return x
+
+
+def sample_action(actor: ActorNetwork, env: SatelliteEnv):
+    # Scale state
+    state = 1 / (env.nb_links - 1) * torch.Tensor(env.state.flatten())
+    action, norm_dist = actor(state)
+    # Clip the action so that the values are in the action space
+    action_clipped = torch.clip(
+        (env.nb_links - 1) * action,
+        min=torch.zeros(3, dtype=torch.int),
+        max=torch.Tensor([env.nb_links - 1, env.nb_links - 1, env.nb_links - 1]),
+    )
+    return action, action_clipped, norm_dist
+
+
+def plot_reward(rewards: list):
+    plt.plot(rewards)
+    plt.xlabel("Time step")
+    plt.ylabel("Cumulated reward")
+    plt.show()
+
+
+def run_actor_critic(links: list, nb_episodes: int, duration_episode: int):
+    env = SatelliteEnv(links)
+    actor = ActorNetwork(obs_size=2 * len(links), action_size=3)
+    critic = CriticNetwork(obs_size=2 * len(links))
+    # Initiliaze loss
+    critic_loss_fn = nn.MSELoss()
+    # Initialize optimizers
+    actor_optimizer = optim.Adam(params=actor.parameters(), lr=LR_ACTOR)
+    critic_optimizer = optim.Adam(params=critic.parameters(), lr=LR_CRITIC)
+    rewards_list = []
+    for _ in range(nb_episodes):
+        env.reset()
+        cumulated_reward = 0
+        try:
+            for j in range(duration_episode):
+                value_state = critic(
+                    1 / (env.nb_links - 1) * torch.Tensor(env.state.flatten())
+                )
+                action, action_clipped, norm_dist = sample_action(actor=actor, env=env)
+                # Observe action and reward
+                next_state, reward, _, _ = env.step(
+                    action_clipped.detach().numpy().astype(int)
+                )
+                cumulated_reward += reward
+                value_next_state = critic(torch.Tensor(next_state.flatten()))
+                target = reward + GAMMA * value_next_state
+                # Calculate losses
+                critic_loss = critic_loss_fn(target, value_state)
+                actor_loss = (
+                    -norm_dist.log_prob(action).unsqueeze(0) * critic_loss.detach()
+                )
+                # Perform backpropagation
+                actor_optimizer.zero_grad()
+                critic_optimizer.zero_grad()
+                actor_loss.backward()
+                critic_loss.backward()
+                actor_optimizer.step()
+                critic_optimizer.step()
+                rewards_list.append(cumulated_reward)
+                if j % 1000 == 0:
+                    logging.info(
+                        "Timestep: {}, Cumulated reward: {}".format(j, cumulated_reward)
+                    )
+                    logging.info("Minimal sum found: {}".format(env.sum_mod_groups_min))
+                    plot_reward(rewards_list)
+
+        except ValueError:
+            # Prevent the algorithm from stopping
+            # if the loss of the actor becomes too
+            # big
+            pass
